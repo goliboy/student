7b71d88b4a950907d515e503e691c12b5b0a93d6
goliboy <mudholhound866@gmail.com>
2025-10-30 15:27:32 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+name=char(input ("enter student number:"))
+age=int(input("enter student age:"))
+usn=int(input("enter student usn:"))
+address=char(input("enter student address:"))
+
+print(f"name:",{sumant})
+print(f"age:",{19})
+print(f"usn:",{01fe24bca184})
+print(f"address:",{sirsi})
